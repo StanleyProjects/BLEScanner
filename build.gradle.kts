a6a458dc60706ee0ebbb4d6ee4b963c1e8d9d05b
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-05-31 14:32:13 +0300

    fix code style

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import sp.gx.core.asFile
 import sp.gx.core.buildDir
 import sp.gx.core.buildSrc
 import sp.gx.core.check
@@ -21,14 +22,44 @@ task<Delete>("clean") {
 task("checkLicense") {
     doLast {
         val author = "Stanley Wintergreen" // todo
-        val report = layout.buildDirectory.get()
+        val report = buildDir()
             .dir("reports/analysis/license")
-            .file("index.html")
-            .asFile
+            .asFile("index.html")
         rootDir.resolve("LICENSE").check(
             expected = emptySet(),
             regexes = setOf("^Copyright 2\\d{3} $author${'$'}".toRegex()),
             report = report,
         )
     }
+}
+
+repositories.mavenCentral()
+
+val ktlint: Configuration by configurations.creating
+
+dependencies {
+    ktlint("com.pinterest:ktlint:${Version.ktlint}") {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
+}
+
+task<JavaExec>("checkCodeStyle") {
+    classpath = ktlint
+    mainClass = "com.pinterest.ktlint.Main"
+    val reporter = "html"
+    val output = buildDir()
+        .dir("reports/analysis/code/style/html")
+        .asFile("index.html")
+    args(
+        "build.gradle.kts",
+        "settings.gradle.kts",
+        "buildSrc/src/main/kotlin/**/*.kt",
+        "buildSrc/build.gradle.kts",
+        "lib/src/main/kotlin/**/*.kt",
+        "lib/src/test/kotlin/**/*.kt",
+        "lib/build.gradle.kts",
+        "--reporter=$reporter,output=${output.absolutePath}",
+    )
 }
