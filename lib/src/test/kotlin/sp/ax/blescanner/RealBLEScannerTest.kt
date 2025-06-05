72c32befa6b27c2e492a314cd51d18e335cf9b50
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-06-06 00:47:24 +0300

    test real BLE scanner

--- lib/src/test/kotlin/sp/ax/blescanner/RealBLEScannerTest.kt
+++ lib/src/test/kotlin/sp/ax/blescanner/RealBLEScannerTest.kt
@@ -1,17 +1,24 @@
 package sp.ax.blescanner
 
 import android.Manifest
+import android.app.Application
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanRecord
+import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.pm.PackageManager
 import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.takeWhile
+import kotlinx.coroutines.flow.toCollection
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeout
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -23,15 +30,20 @@ import kotlin.time.Duration.Companion.seconds
 
 @RunWith(RobolectricTestRunner::class)
 internal class RealBLEScannerTest {
+    private fun before(application: Application): BluetoothManager {
+        val bm = application.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        Shadows.shadowOf(bm.adapter).setState(BluetoothAdapter.STATE_ON)
+        check(bm.adapter.isEnabled)
+        Shadows.shadowOf(application).grantPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+        check(application.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        return bm
+    }
+
     @Test
     fun startTest() {
         runTest(timeout = 6.seconds) {
             val application = RuntimeEnvironment.getApplication()
-            val bm = application.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-            Shadows.shadowOf(bm.adapter).setState(BluetoothAdapter.STATE_ON)
-            check(bm.adapter.isEnabled)
-            Shadows.shadowOf(application).grantPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
-            check(application.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            before(application = application)
             onRealBLEScanner(context = application) { scanner ->
                 launch(CoroutineName("errors")) {
                     scanner.errors.take(1).collect { error ->
@@ -56,11 +68,7 @@ internal class RealBLEScannerTest {
     fun startStopTest() {
         runTest(timeout = 6.seconds) {
             val application = RuntimeEnvironment.getApplication()
-            val bm = application.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-            Shadows.shadowOf(bm.adapter).setState(BluetoothAdapter.STATE_ON)
-            check(bm.adapter.isEnabled)
-            Shadows.shadowOf(application).grantPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
-            check(application.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            before(application = application)
             onRealBLEScanner(context = application) { scanner ->
                 launch(CoroutineName("errors")) {
                     scanner.errors.take(1).collect { error ->
@@ -88,48 +96,73 @@ internal class RealBLEScannerTest {
         }
     }
 
+    private inline fun <reified T : Any> Class<out Any>.invoke(name: String, argument: Any): T {
+        val method = methods.single { it.name == name }
+        if (method == null) TODO()
+        return method.invoke(null, argument) as T
+    }
+
     @Test
     fun devicesTest() {
-        runTest(timeout = 6.seconds) {
-            val application = RuntimeEnvironment.getApplication()
-            val devices = (1..3).map { number ->
-                BLEDevice(
-                    name = "name:$number",
-                    address = "address:$number",
-                    bytes = byteArrayOf(number.toByte()),
-                )
-            }
-            onRealBLEScanner(context = application) { scanner ->
-                launch(CoroutineName("errors")) {
-                    scanner.errors.take(1).collect { error ->
-                        error("Error $error is unexpected!")
-                    }
-                }.cancel {
-                    assertEquals("before start", BLEScanner.State.Stopped, scanner.states.value)
-                    launch(CoroutineName("start")) {
-                        scanner.states.takeWhile { state ->
-                            state != BLEScanner.State.Started
-                        }.collect()
-                    }.join {
-                        launch(CoroutineName("devices")) {
-                            scanner.devices.take(devices.size).collectIndexed { index, actual ->
-                                if (index !in devices.indices) error("Index $index is unexpected!")
-                                val expected = devices[index]
-                                assertEquals(expected.name, actual.name)
-                                assertEquals(expected.address, actual.address)
-                                assertTrue(expected.bytes.contentEquals(actual.bytes))
-                            }
-                        }.join {
-                            scanner.start()
+        runBlocking {
+            withTimeout(6.seconds) {
+                val application = RuntimeEnvironment.getApplication()
+                val bm = before(application = application)
+                val devices = (0..5).map { number ->
+                    BLEDevice(
+                        name = "name:$number",
+                        address = "AA:BB:CC:DD:EE:F" + ('A'.code + number).toChar(),
+                        bytes = byteArrayOf(number.toByte()),
+                    )
+                }.sortedBy { it.address }
+                onRealBLEScanner(
+                    main = coroutineContext,
+                    context = application,
+                ) { scanner ->
+                    launch(CoroutineName("errors")) {
+                        scanner.errors.take(1).collect { error ->
+                            error("Error $error is unexpected!")
                         }
-                    }
-                    assertEquals("after start", BLEScanner.State.Started, scanner.states.value)
-                    launch(CoroutineName("stop")) {
-                        scanner.states.takeWhile { state ->
-                            state != BLEScanner.State.Stopped
-                        }.collect()
-                    }.join {
-                        scanner.stop()
+                    }.cancel {
+                        assertEquals("before start", BLEScanner.State.Stopped, scanner.states.value)
+                        launch(CoroutineName("start")) {
+                            scanner.states.takeWhile { state ->
+                                state != BLEScanner.State.Started
+                            }.collect()
+                        }.join {
+                            launch(CoroutineName("devices")) {
+                                scanner.devices.take(devices.size).toCollection(ArrayList())
+                                    .sortedBy { it.address }.forEachIndexed { index, actual ->
+                                    if (index !in devices.indices) error("Index $index is unexpected!")
+                                    val expected = devices[index]
+                                    assertEquals(expected.name, actual.name)
+                                    assertEquals(expected.address, actual.address)
+                                    assertTrue(expected.bytes.contentEquals(actual.bytes))
+                                }
+                            }.join {
+                                scanner.start()
+                                devices.forEach { device ->
+                                    val record: ScanRecord =
+                                        ScanRecord::class.java.invoke<ScanRecord>(
+                                            "parseFromBytes",
+                                            device.bytes
+                                        )
+                                    val bd = bm.adapter.getRemoteDevice(device.address)
+                                    Shadows.shadowOf(bd).setName(device.name)
+                                    val result = ScanResult(bd, record, 1, 1)
+                                    Shadows.shadowOf(bm.adapter.bluetoothLeScanner)
+                                        .addScanResult(result)
+                                }
+                            }
+                        }
+                        assertEquals("after start", BLEScanner.State.Started, scanner.states.value)
+                        launch(CoroutineName("stop")) {
+                            scanner.states.takeWhile { state ->
+                                state != BLEScanner.State.Stopped
+                            }.collect()
+                        }.join {
+                            scanner.stop()
+                        }
                     }
                 }
             }
--- lib/src/test/kotlin/sp/ax/blescanner/Util.kt
+++ lib/src/test/kotlin/sp/ax/blescanner/Util.kt
@@ -7,9 +7,7 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -23,25 +21,6 @@ internal suspend fun Job.cancel(delay: Duration = 1.seconds, preCancel: suspend 
     delay(delay)
     preCancel()
     cancel()
-}
-
-@Deprecated(message = "TestScope.onMockScanner")
-internal suspend fun onMockScannerOld(
-    main: CoroutineContext = MockEnvironment.main,
-    default: CoroutineContext = MockEnvironment.default,
-    defaultState: BLEScanner.State = BLEScanner.State.Stopped,
-    devices: List<BLEDevice> = emptyList(),
-    block: suspend (BLEScanner) -> Unit,
-) {
-    val job = SupervisorJob()
-    val scanner = MockScanner(
-        coroutineScope = CoroutineScope(main + job),
-        default = default + job,
-        defaultState = defaultState,
-        expected = devices,
-    )
-    block(scanner)
-    job.cancel()
 }
 
 internal suspend fun TestScope.onMockScanner(
@@ -62,33 +41,28 @@ internal suspend fun TestScope.onMockScanner(
     job.cancel()
 }
 
-@Deprecated(message = "TestScope.onRealBLEScanner")
-internal suspend fun onRealBLEScannerOld(
-    main: CoroutineContext = EmptyCoroutineContext,
-    default: CoroutineContext = MockEnvironment.default,
-    context: Context,
-    timeout: Duration = 10.seconds,
-    block: suspend (BLEScanner) -> Unit,
-) {
-    val job = SupervisorJob()
-    val scanner = RealBLEScanner(
-        coroutineScope = CoroutineScope(main + job),
-        default = default + job,
-        context = context,
-        timeout = timeout,
-    )
-    block(scanner)
-    job.cancel()
-}
-
 internal suspend fun TestScope.onRealBLEScanner(
     context: Context,
     timeout: Duration = 10.seconds,
     block: suspend (BLEScanner) -> Unit,
 ) {
+    onRealBLEScanner(
+        main = StandardTestDispatcher(testScheduler, "real:scanner:main"),
+        default = StandardTestDispatcher(testScheduler, "real:scanner:main"),
+        context = context,
+        timeout = timeout,
+        block = block,
+    )
+}
+
+internal suspend fun onRealBLEScanner(
+    main: CoroutineContext,
+    default: CoroutineContext = main,
+    context: Context,
+    timeout: Duration = 10.seconds,
+    block: suspend (BLEScanner) -> Unit,
+) {
     val job = SupervisorJob()
-    val main = StandardTestDispatcher(testScheduler, "main")
-    val default = StandardTestDispatcher(testScheduler, "default")
     val scanner = RealBLEScanner(
         coroutineScope = CoroutineScope(main + job),
         default = default + job,
